7c02e6df5a80fde2a0e97515daf744e25a403d45
nhyeonjeong <hjnam5962@gmail.com>
2024-01-08 21:26:48 +0900

    오토레이아웃, struct배열로 tableView구현

---
+++ TravelMagazineProject/TravelTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  TravelTableViewCell.swift
+//  TravelMagazineProject
+//
+//  Created by 남현정 on 2024/01/08.
+//
+
+import UIKit
+
+class TravelTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var magazineImageView: UIImageView!
+    @IBOutlet weak var magazineTitle: UILabel!
+    @IBOutlet weak var magazineSubTitle: UILabel!
+    @IBOutlet weak var magazineDate: UILabel!
+    
+}
